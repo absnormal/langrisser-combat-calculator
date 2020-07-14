5a433eac433a0b6e28fa3b5b20904224ad558223
Absnormal <absnormal26065873@gmail.com>
2020-07-14 22:59:03 +0800

    dev version 0.2.1 framework complete

--- js/control.js
+++ js/control.js
@@ -8,7 +8,23 @@ function refresh(){
 
 // display version
 function display_version(){
-    var ver = "0.1.3";
+    var ver = "0.2.1";
     document.getElementById("version").innerHTML = "version " + ver;
 };
 
+// display local path
+function display_path(path){
+    var local='file:///home/absnormal/';
+    var rep = 'langrisser-combat-calculator/';
+    var functs = ['overall', 'testing', 'baseatk', 'preatk', 'fixatk', 'counter', 'predef', 'fixdef', 'terrain', 'rate'];
+
+    var i;
+    for(i = 0; i<functs.length; i++){
+        if(path == 'local'){
+            document.getElementById(functs[i]).href = local + rep + functs[i] + '.html';
+        }
+        else{
+            document.getElementById(functs[i]).href = rep + functs[i];
+        }
+    }
+};
