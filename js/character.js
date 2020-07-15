ca36ff73b8ad9335190d5b3e031d46b5a8259d33
Absnormal <absnormal26065873@gmail.com>
2020-07-16 01:25:36 +0800

    dev version 0.2.3 click-char-display-skill

--- js/character.js
+++ js/character.js
@@ -1,8 +1,23 @@
 var selected = 'no';
 
+function displaySkill(charName){
+    var skills = document.getElementsByClassName(charName);
+    for(var i=0; i<skills.length; i++){
+        skills[i].style = '';
+    }
+};
+
+function hideSkill(){
+    var skills = document.getElementsByClassName('skill');
+    for(var i=0; i<skills.length; i++){
+        skills[i].style = 'display: none;';
+    }
+};
+
 function selectChar(charName){
     if(document.getElementById(charName).classList.contains('selected')){
         document.getElementById(charName).classList.remove('selected');
+        hideSkill();
         selected = 'no';
     }
     else{
@@ -15,6 +30,9 @@ function selectChar(charName){
             }
         }
         document.getElementById(charName).classList.add('selected');
+        hideSkill();
+        displaySkill(charName);
         selected = 'yes';
     }
 };
+
