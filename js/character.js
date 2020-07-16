24dfad7bb0a559ccfad702de6bab5273c95341a3
Absnormal <absnormal26065873@gmail.com>
2020-07-16 13:11:30 +0800

    dev version 0.2.4 add-party first-display-skill github

--- js/character.js
+++ js/character.js
@@ -1,4 +1,4 @@
-var selected = 'no';
+var selectedChar = 'Rachel', selectedParty = 'Chosen';
 
 function displaySkill(charName){
     var skills = document.getElementsByClassName(charName);
@@ -14,25 +14,44 @@ function hideSkill(){
     }
 };
 
+function displayChar(partyName){
+    var chars = document.getElementsByClassName(partyName);
+    for(var i=0; i<chars.length; i++){
+        chars[i].style = '';
+    }
+};
+
+function hideChar(){
+    var chars = document.getElementsByClassName('character');
+    for(var i=0; i<chars.length; i++){
+        chars[i].style = 'display: none;'
+    }
+};
+
 function selectChar(charName){
-    if(document.getElementById(charName).classList.contains('selected')){
-        document.getElementById(charName).classList.remove('selected');
-        hideSkill();
-        selected = 'no';
+    // de-select old char
+    if(document.getElementById(selectedChar).classList.contains('selected')){
+        document.getElementById(selectedChar).classList.remove('selected');
     }
-    else{
-        if(selected == 'yes'){
-            var chars = document.getElementsByClassName('character');
-            for(var i=0; i<chars.length; i++){
-                if(chars[i].classList.contains('selected')){
-                    chars[i].classList.remove('selected');
-                }
-            }
-        }
-        document.getElementById(charName).classList.add('selected');
-        hideSkill();
-        displaySkill(charName);
-        selected = 'yes';
+    // select new char
+    document.getElementById(charName).classList.add('selected');
+    selectedChar = charName;
+    // hide all skills and display skills in char
+    hideSkill();
+    displaySkill(charName);
+};
+
+function selectParty(partyName){
+    // de-select old party
+    if(document.getElementById(selectedParty).classList.contains('selected')){
+        document.getElementById(selectedParty).classList.remove('selected');
     }
+    // select new party & char
+    document.getElementById(partyName).classList.add('selected');
+    selectedParty = partyName;
+    selectChar(document.getElementsByClassName(partyName)[0].id);
+    // hide all chars and display all chars in party
+    hideChar();
+    displayChar(partyName);
 };
 
