ddb860fdc05d46d2e3fd9443cc749b705eb70f68
Absnormal <absnormal26065873@gmail.com>
2021-02-10 17:42:28 +0800

    [test] branch name asset(X) assets(O)

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var local = "file:///home/absnormal/langrisser-combat-calculator/";
  */
 /* github
  */
-var imgLocal = "https://raw.githubusercontent.com/absnormal/langrisser-combat-calculator/asset/image/";
+var imgLocal = "https://raw.githubusercontent.com/absnormal/langrisser-combat-calculator/assets/image/";
 var local = "/langrisser-combat-calculator/";
 var page = {
     ATK: {
